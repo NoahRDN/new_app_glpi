93cb4982f22c6b52e66369525793514a3cd28b66
Noah <noahmichaelrandrianah@gmail.com>
2026-06-06 02:09:56 +0300

    create assets entities

--- src/entities/asset/model/asset.types.ts
+++ src/entities/asset/model/asset.types.ts
@@ -1,10 +1,5 @@
-export type AssetStatus = "in_service" | "maintenance" | "retired";
-
 export type Asset = {
-  assignedTo: string;
-  id: number;
-  location: string;
-  name: string;
-  status: AssetStatus;
-  type: "Laptop" | "Printer" | "Switch";
+  itemtype: string,
+  name: string,
+  href: string
 };
